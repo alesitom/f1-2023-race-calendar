fcf512e8c273c3f89483cd8af7925a4e00f5a74f
alesitom <alemendoza1990@gmail.com>
2023-03-04 19:36:45 -0300

    App.js rewrited to check stylesheet.css, added css styles for font and background color, added data.js and some dummy data

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const calendarData = [
+    {
+        id:"1",
+        date: "05/03/2023",
+        yime: "12:00",
+        name: "Bahrain",
+        type: "Race",
+        circuit: "Bahrain International Circuit in Sakhir",
+        country:"Bahrain",
+        description: "Formula 1 Gulf Air Bahrain Grand Prix 2023",
+        laps:"57"
+    },
+    {
+        id:"2",
+        date: "19/03/2023",
+        time: "14:00",
+        name: "Arabia Saudita",
+        type: "Race",
+        circuit: "Jeddah Corniche Circuit",
+        country:"Arabia Saudita",
+        description: "Formula 1 STC Saudi Arabian Grand Prix 2023",
+        laps:"57"
+    }
+]
